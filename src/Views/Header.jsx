f7376414de2736f712108a290d8b67c69c61b59b
hashonedev <hashonedev@gmail.com>
2020-07-01 13:31:40 +0500

    responsive changes header and app icon changed

--- src/Views/Header.jsx
+++ src/Views/Header.jsx
@@ -4,11 +4,15 @@ import 'bootstrap/dist/css/bootstrap.css';
 import '../App.css';
 import '../Style.css';
 import logo from '../logo.png';
-import FontAwesome from 'react-fontawesome'
+import FontAwesome from 'react-fontawesome';
+
+import { Nav, Navbar, Form, FormControl } from 'react-bootstrap';
+
 import {
     Container,
     Row,
-    Col
+    Col,
+    Button
 } from "reactstrap";
 import {Link, useRouteMatch, useParams } from 'react-router-dom';
 
@@ -17,10 +21,18 @@ class Header extends React.Component{
     super(props);
     this.activeRoute.bind(this);
     console.log("Header is here", props.history);
+    this.state = {
+      toggle:false
+    }
   }
   activeRoute(routeName) {
     return this.props.location.pathname.indexOf(routeName) > -1 ? "active" : "";
   }
+
+  Toggle = () => {
+    this.setState({toggle:!this.state.toggle})
+}
+
     render(){
 
         let userObj =  JSON.parse(localStorage.getItem("user-dostava")) ;
@@ -84,21 +96,45 @@ class Header extends React.Component{
   
   
             </Row>
-            <Row className="mainHeader">
-              <Col lg="3" className="logo">
+            <Row className="mainHeader ">
+              <Navbar sticky="top" bg="light" expand="lg" fluid>
+              
+                  <Navbar.Brand lg="3" href="#home"><img src={logo} alt="Logo" /></Navbar.Brand>
+                
+                  <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                  <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav className="mr-auto ml-auto menuitems">
+                      <ul>{MenuList}</ul>
+                    </Nav>
+                  </Navbar.Collapse>
+                  <Form inline className="text-right">
+                    <FormControl type="text" placeholder="Search" className="mr-sm-2" />
+                    <Button variant="outline-success">Search</Button>
+                  </Form>
+                  <div className="cart-icon-header">
+                  <Link to="/cart">
+                    <FontAwesome name="shopping-bag"/>
+                    <strong>15</strong>
+                  </Link>
+                  </div>
+                
+</Navbar>
+              {/* <Col lg="3" className="logo">
                 <Link to="/">
                   <img src={logo} alt="Logo" />
                 </Link>
               </Col>
               <Col lg="6" className="menuitems">
+              
                 <ul>{MenuList}</ul>
-              </Col>
-              <Col lg="3" className="cart-icon-header text-right">
+              
+              </Col> */}
+              {/* <Col lg="3" className="cart-icon-header text-right">
                   <Link to="/cart">
                     <FontAwesome name="shopping-bag"/>
                     <strong>15</strong>
                   </Link>
-              </Col>
+              </Col> */}
             </Row>
           </Container>
 
