83fd0b2ee245e616be96c64f53816e0fb104bc00
sadruddin <sadruddin1502a@aptechgdn.net>
2020-12-31 17:39:20 +0500

    C capital

--- src/Views/Vendor.jsx
+++ src/Views/Vendor.jsx
@@ -26,7 +26,7 @@ import PlacesAutocomplete, {
   geocodeByAddress,
   getLatLng,
 } from 'react-places-autocomplete';
-import FlashAlert from "../components/FlashAlert.jsx";
+import FlashAlert from "../Components/FlashAlert.jsx";
 
 const cache = new InMemoryCache()
 const httpLink = createUploadLink({
