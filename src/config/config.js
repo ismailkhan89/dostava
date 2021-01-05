e275411d2849bbf64d9e97f153ffa6f1f98ebaf3
hashonedev <hashonedev@gmail.com>
2021-01-05 17:20:54 +0500

    image changed

--- src/config/config.js
+++ src/config/config.js
@@ -10,7 +10,7 @@ export const cloudinary_profile = "nh9fd8i5"
 
 
 export const STRIPE_PUBLIC_KEY  = "pk_live_51HMUqLE2mcGFzQylsGoHF593qswX3iGs4ON2b8TOzlTmWDCMVH2ESUJ7bdp3Lu5BtvRt4B2PrcGB0ogfdEcZ0HO600LvoH34ja"
-export const STRIPE_IMAGE_URL   = "https://api.dostava.com.au/assets/images/dostava/logo.png"
+export const STRIPE_IMAGE_URL   = "https://api.dostava.com.au/assets/images/dostava/dostava-new.png"
 export const STRIPE_STORE_NAME  = "Dostava"
 
 export const WS_GRAPHQL_URL = 'wss://api.dostava.com.au/graphql'
